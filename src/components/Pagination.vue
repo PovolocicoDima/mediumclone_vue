1c214f63f422446aab727aa697e0a0443a22b32f
Поволочко Дмитрий <povolochko@reg.ru>
2022-03-01 17:06:34 +0200

    first commit

---
+++ src/components/Pagination.vue
@@ -0,0 +1,49 @@
+<template>
+    <ul class="pagination">
+        <li 
+            v-for="page in pages"
+            :key="page"    
+            class="page-item"
+            :class="{ active: currentPage === page }"
+        >
+            <router-link 
+            :to="{path: url, query: {page: page}}" 
+            class="page-link"
+            >
+                {{ page }}
+            </router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+import {range} from '@/helpers/utils';
+
+export default {
+    name: 'McvPagination',
+    props: {
+        total: {
+            type: Number,
+            requered: true
+        },
+        limit: {
+            type: Number,
+            requered: true
+        },
+        currentPage: {
+            type: Number,
+            requered: true
+        },
+        url: {
+            type: String,
+            requered: true
+        },
+    },
+    computed: {
+        pages() {
+            const pagesCount = Math.ceil(this.total / this.limit);
+            return range(1, pagesCount);
+        }
+    }
+}
+</script>
---
+++ src/views/GlobalFeed.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="home-page">
+        Banner
+        <div class="container page">
+            <div class="row">
+                <div class="col-md-9">
+                    <mcv-feed :api-url="apiUrl" />
+                </div>
+                <div class="col-md-3">
+                    <mcv-popular-tags />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import McvFeed from '@/components/Feed';
+import McvPopularTags from '@/components/PopularTags';
+
+export default {
+    name: 'McvGlobalFeed',
+    components: {
+        McvFeed,
+        McvPopularTags
+    },
+    data() {
+        return {
+            apiUrl: '/articles'
+        }
+    }
+}
+</script>
